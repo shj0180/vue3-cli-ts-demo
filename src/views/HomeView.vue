8c29b40cbfa1cb6263200fa219f7338afc8892aa
shj0180 <45070318+shj0180@users.noreply.github.com>
2023-02-07 09:03:16 +0800

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,130 @@
 <template>
-  <!-- <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div> -->
+    <el-container>
+      <el-header>
+        <el-row :gutter="20">
+          <el-col :span="4">
+            <img src="@/assets/4.png" class="plat-logo"/>
+          </el-col>
+          <el-col :span="16">
+            <h2>STG4应用管理平台</h2>
+          </el-col>
+          <el-col :span="4" class="user_info">
+            用户信息
+          </el-col>
+        </el-row>
+      </el-header>
 
-  <el-button type="primary">Primary</el-button>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            active-text-color="#ffd04b"
+            background-color="#545c64"
+            class="el-menu-vertical-demo"
+            default-active="2"
+            text-color="#fff"
+          >
+            <el-sub-menu index="1">
+              <template #title>
+                <!-- <el-icon><location /></el-icon> -->
+                <span>应用管理</span>
+              </template>
+              
+              <el-menu-item index="1-1">
+                <!-- <el-icon><location /></el-icon> -->
+                应用版本
+              </el-menu-item>
+              <el-menu-item index="1-2">
+                <!-- <el-icon><icon-menu /></el-icon> -->
+                应用配置
+              </el-menu-item>
+              <el-menu-item index="1-3">
+                <!-- <el-icon><icon-menu /></el-icon> -->
+                应用重启
+              </el-menu-item>
+              <el-sub-menu index="1-4">
+                <template #title>应用环境构建</template>
+                <el-menu-item index="1-4-1">过保替换</el-menu-item>
+                <el-menu-item index="1-4-2">全新应用</el-menu-item>
+              </el-sub-menu>
+
+              
+
+            </el-sub-menu>
+
+            <el-sub-menu>
+              <template #title>
+                <!-- <el-icon><location /></el-icon> -->
+                <span>数据库管理</span>
+              </template>
+
+              <el-menu-item index="2-1">
+                <!-- <el-icon><icon-menu /></el-icon> -->
+                <span>版本管理</span>
+              </el-menu-item>
+            </el-sub-menu>
+
+            <el-sub-menu>
+              <template #title>
+                <!-- <el-icon><location /></el-icon> -->
+                <span>Prometheus</span>
+              </template>
+
+              <el-menu-item index="3-1">
+                <!-- <el-icon><icon-menu /></el-icon> -->
+                <span>一键配置</span>
+              </el-menu-item>
+
+              <el-menu-item index="3-2">
+                <!-- <el-icon><icon-menu /></el-icon> -->
+                <span>监控数据</span>
+              </el-menu-item>
+
+            </el-sub-menu>
 
 
+          
+          </el-menu>
+        </el-aside>
+
+        <el-main>
+          Main
+        </el-main>
+      </el-container>
+    </el-container>
 </template>
 
-<!-- <script lang="ts">
-// import { defineComponent } from 'vue';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-// export default defineComponent({
-//   name: 'HomeView',
-//   components: {
-//     HelloWorld,
-//   },
-// });
-</script> -->
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style lang='scss' scoped>
+  
+.el-header{
+  height: 100px;
+  background-color: rgb(153, 181, 235);
+  .plat-logo{
+    height: 100px;
+  }
+  h2, .user_info{
+    text-align: center;
+    height: 100px;
+    line-height: 100px;
+  }
+}
+.el-aside{
+  .el-menu{
+    height: calc(100vh - 100px);
+  }
+  
+}
+  
+</style>
